c93be9fbb2e932c8f00359954b541a68c5808507
kumailnaqvi354 <komailnaqvi616@gmail.com>
2021-07-13 18:29:54 +0500

    contract development complete

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -21,4 +21,50 @@ contract Wallet is Compound {
         );
         supply(cTokenAddress, underlyingAmount);
     }
+
+    function withdraw(
+        address cTokenAddress,
+        uint256 underlyingAmount,
+        address recipient
+    ) external onlyAdmin() {
+
+        require(getUnderlyingBalance(cTokenAddress) >= underlyingAmount,'balance too low');
+        claimComp();
+        redeem(cTokenAddress, underlyingAmount);
+
+        address underlyingAddress = getUnderlyingAddress(cTokenAddress);
+        IERC20(cTokenAddress).transfer(recipient, underlyingAmount);
+
+        address compAddress = getCompAddress();
+        IERC20 compToken = IERC20(compAddress);
+        uint compAmount =  compToken.balanceOf(address(this));
+        compToken.
+        transfer(recipient, compAmount);
+    }
+
+    function withdrawEth(
+        uint underlyingAmount,
+        address payable recipient
+    )onlyAdmin() external{
+        require(getUnderlyingEthBalance() >= underlyingAmount,'balance too low');
+        claimComp();
+        redeemEth(underlyingAmount);
+        
+        recipient.transfer(underlyingAmount);
+
+        address compAddress = getCompAddress();
+        IERC20 compToken = IERC20(compAddress);
+        uint compAmount = compToken.balanceOf(address(this));
+        compToken.transfer(recipient, compAmount);
+
+    }
+
+    receive() external payable {
+        supplyEth(msg.value);
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "only admin");
+        _;
+    }
 }
